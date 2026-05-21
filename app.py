80fa48368bd47ecb1239c8d1f38a52cb1f287b37
Ananya03 <ananya03ann@gmail.com>
2026-05-21 12:02:28 +0530

    Configured Streamlit dashboard page settings

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ import pandas as pd
 import plotly.express as px
 from sqlalchemy import create_engine
 
+# ==============================
+# PAGE CONFIG
+# ==============================
+
+st.set_page_config(
+    page_title="News Sentiment Dashboard",
+    page_icon="📰",
+    layout="wide"
+)

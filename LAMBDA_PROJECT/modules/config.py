e3f724169a82d0d5485682db4c6f02617289d0c4
Ananya03 <ananya03ann@gmail.com>
2026-05-21 12:29:21 +0530

    Added configuration module for Lambda project

---
+++ LAMBDA_PROJECT/modules/config.py
@@ -0,0 +1,11 @@
+# DATABASE CONFIGURATION
+
+DB_HOST = "database-1.c3qm2yq0clyo.ap-south-1.rds.amazonaws.com"
+DB_NAME = "newsdb"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres-123"
+DB_PORT = 5432
+
+# NEWS API
+
+NEWS_API_KEY = "09c5ffa098854520935f6121e5f146d8"
